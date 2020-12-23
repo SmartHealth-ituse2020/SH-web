e814b40fc9878b301d21661baa8bb41eac05dd90
Gökay Temizkan <temizkang16@itu.edu.tr>
2020-12-23 21:51:04 +0300

    url rule for doctor login page

--- server.py
+++ server.py
@@ -7,9 +7,10 @@ import views, doctor_views, dboperations
 app=Flask(__name__)
 app.config["DEBUG"] = True
     
-app.add_url_rule("/doctor", methods=["GET", "POST"], view_func=views.home_page_doctor)
+app.add_url_rule("/doctor", methods=["GET", "POST"], view_func=doctor_views.home_page_doctor)
 app.add_url_rule("/", view_func=views.login_page)
 app.add_url_rule("/login", view_func=views.login_page)
+app.add_url_rule("/login/doctor", methods=["GET", "POST"], view_func=doctor_views.login_page_doctor)
 
 if __name__ == "__main__":
     app.run()
