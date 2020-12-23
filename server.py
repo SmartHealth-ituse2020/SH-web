c3f0d819bd7fbcce2d000b0c28cc057132e9e462
Gökay Temizkan <temizkang16@itu.edu.tr>
2020-12-23 20:28:13 +0300

    "/" forwards to login_page

--- server.py
+++ server.py
@@ -2,12 +2,13 @@ from flask import Flask
 
 import psycopg2 as dbapi2
         
-import views
+import views, doctor_views, dboperations
 
 app=Flask(__name__)
 app.config["DEBUG"] = True
     
 app.add_url_rule("/doctor", methods=["GET", "POST"], view_func=views.home_page_doctor)
+app.add_url_rule("/", view_func=views.login_page)
 app.add_url_rule("/login", view_func=views.login_page)
 
 if __name__ == "__main__":
