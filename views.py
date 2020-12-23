8573732dad928d85c3f0e9a776727856ef3d0dfc
Gökay Temizkan <temizkang16@itu.edu.tr>
2020-12-23 20:26:50 +0300

    views_py has common pages

--- views.py
+++ views.py
@@ -6,22 +6,9 @@ import os
 
 import psycopg2 as dbapi2
 
+import dboperations
+
 # DATABASE_URL = os.environ['DATABASE_URL']
-
-# def query(url, table_name):
-#     with dbapi2.connect(url) as connection:
-#         cursor = connection.cursor()
-#         cursor.execute("SELECT * FROM %s" % (table_name))
-#         rows = cursor.fetchall()
-        
-#         cursor.close()
-#         return rows
-
-def home_page_doctor():
-    today = datetime.today()
-    day_name = today.strftime("%A")
-
-    return render_template("home_doctor.html", day=day_name)
 
 def login_page():
      return render_template("login.html")
