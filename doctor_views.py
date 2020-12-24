ec8de753c9ce8e4baf524f1afccb37b207778fc9
Emek Gözlüklü <emekgzlkl@gmail.com>
2020-12-25 01:50:03 +0300

    add form action and fix login

--- doctor_views.py
+++ doctor_views.py
@@ -9,19 +9,22 @@ import dboperations
 # DATABASE_URL = os.environ['DATABASE_URL']
 DATABASE_URL = "postgres://fvtkacqijrpxfk:9af8c01dc052361fd51630b634e9a1df34106a7eae0ac736c6a36099bb476d87@ec2-54-246-115-40.eu-west-1.compute.amazonaws.com:5432/d4qj8s0lt8sev8"
 
+
+def home_page_doctor():
+    rows = dboperations.query(DATABASE_URL, "patient")
+#     rows={"...", "...", "...", "...", "...", "..."}
+    return render_template("home_doctor.html", rows=sorted(rows), len=len(rows))
+
+
 def login_page_doctor():
     form = DoctorLoginForm()  # request.form)
     if form.validate_on_submit():
         # user = User.query.filter_by(email=form.email.data).first()
         # if user is not None and user.verify_password(form.password.data):
         # login_user(None)
-        return redirect("home_page_doctor")
+        return redirect(url_for("home_page_doctor"))
     return render_template('login_doctor.html', form=form)
 
-def home_page_doctor():
-    rows = dboperations.query(DATABASE_URL, "patient")
-#     rows={"...", "...", "...", "...", "...", "..."}
-    return render_template("home_doctor.html", rows=sorted(rows), len=len(rows))
 
 def add_patient_page_doctor():
     
--- forms.py
+++ forms.py
@@ -15,3 +15,16 @@ class DoctorLoginForm(FlaskForm):
 
     def __init__(self, *args, **kwargs):
         super(DoctorLoginForm, self).__init__(*args, **kwargs)
+
+class DoctorLoginForm(FlaskForm):
+    national_id = StringField('National ID',
+                              validators=[DataRequired(), Length(8, 15)],
+                              render_kw={'class': 'input'}
+                              )
+
+    password = PasswordField('Password', validators=[DataRequired()], render_kw={'class': 'input'})
+    remember_me = BooleanField('Remember me')
+    submit = SubmitField('Log In', render_kw={'class': 'button is-success'})
+
+    def __init__(self, *args, **kwargs):
+        super(DoctorLoginForm, self).__init__(*args, **kwargs)
