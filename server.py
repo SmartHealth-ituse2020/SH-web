5d5bcd644a50292d89133df0e5c9c17bc2982ce9
Emek Gözlüklü <emekgzlkl@gmail.com>
2020-12-25 00:34:37 +0300

    add login form backend

--- server.py
+++ server.py
@@ -3,9 +3,12 @@ from flask import Flask
 import psycopg2 as dbapi2
         
 import views, doctor_views, dboperations
+import os
+SECRET_KEY = os.urandom(32)
 
 app=Flask(__name__)
 app.config["DEBUG"] = True
+app.config['SECRET_KEY'] = SECRET_KEY
     
 app.add_url_rule("/doctor", methods=["GET", "POST"], view_func=doctor_views.home_page_doctor)
 app.add_url_rule("/", view_func=views.login_page)
