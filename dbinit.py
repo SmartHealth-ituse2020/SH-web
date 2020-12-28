fbf915cca97b68bc25492a3250514a0d9dd785d0
Gökay Temizkan <temizkang16@itu.edu.tr>
2020-12-28 14:47:07 +0300

    DATABASE_URL is given in code temprorarily

--- dbinit.py
+++ dbinit.py
@@ -1,7 +1,8 @@
 import psycopg2 as dbapi2
 import os
 
-DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = "postgres://fvtkacqijrpxfk:9af8c01dc052361fd51630b634e9a1df34106a7eae0ac736c6a36099bb476d87@ec2-54-246-115-40.eu-west-1.compute.amazonaws.com:5432/d4qj8s0lt8sev8"
+#DATABASE_URL = os.environ['DATABASE_URL']
 INIT_STATEMENTS = [open("database_design.sql", "r").read()]
 
 
