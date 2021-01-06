9d03606f54b98e2637bc452a90c51498937af1f3
Emek Gözlüklü <gozluklu16@itu.edu.tr>
2021-01-07 02:33:32 +0300

    organize login forms

--- smarthealth_web/forms.py
+++ smarthealth_web/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField, Radio
 from wtforms.validators import DataRequired, Length
 
 
-class DoctorLoginForm(FlaskForm):
+class LoginForm(FlaskForm):
     national_id = StringField('National ID',
                               validators=[DataRequired(), Length(8, 15)],
                               render_kw={'class': 'input'}
@@ -12,34 +12,24 @@ class DoctorLoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()], render_kw={'class': 'input'})
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Log In', render_kw={'class': 'button is-success'})
+
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+
+
+class DoctorLoginForm(LoginForm):
 
     def __init__(self, *args, **kwargs):
         super(DoctorLoginForm, self).__init__(*args, **kwargs)
 
 
-class PatientLoginForm(FlaskForm):
-    national_id = StringField('National ID',
-                              validators=[DataRequired(), Length(8, 15)],
-                              render_kw={'class': 'input'}
-                              )
-
-    password = PasswordField('Password', validators=[DataRequired()], render_kw={'class': 'input'})
-    remember_me = BooleanField('Remember me')
-    submit = SubmitField('Log In', render_kw={'class': 'button is-success'})
+class PatientLoginForm(LoginForm):
 
     def __init__(self, *args, **kwargs):
         super(PatientLoginForm, self).__init__(*args, **kwargs)
 
 
-class AdminLoginForm(FlaskForm):
-    national_id = StringField('National ID',
-                              validators=[DataRequired(), Length(8, 15)],
-                              render_kw={'class': 'input'}
-                              )
-
-    password = PasswordField('Password', validators=[DataRequired()], render_kw={'class': 'input'})
-    remember_me = BooleanField('Remember me')
-    submit = SubmitField('Log In', render_kw={'class': 'button is-success'})
+class AdminLoginForm(LoginForm):
 
     def __init__(self, *args, **kwargs):
         super(AdminLoginForm, self).__init__(*args, **kwargs)
