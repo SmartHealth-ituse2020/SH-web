1ef3a933fd7fe6da4cee58d9f6adbdd185a48c4c
Emek Gözlüklü <emekgzlkl@gmail.com>
2021-01-11 02:23:47 +0300

    implement login required and logout required decorators

--- smarthealth_web/views.py
+++ smarthealth_web/views.py
@@ -1,13 +1,32 @@
-# from datetime import datetime
-# import psycopg2 as dbapi2
-# from flask import request, redirect, url_for
-# import dboperations
-from flask import render_template
+from flask import render_template, session, redirect, url_for, flash
 from decouple import config
+import functools
 
-# DATABASE_URL = os.environ.get('DATABASE_URL')
 DATABASE_URL = config('DATABASE_URL')
 
 
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if 'user_id' not in session:
+            flash("Please login first.")
+            return redirect(url_for("login_page"))
+        return view(**kwargs)
+    return wrapped_view
+
+
+def logout_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if 'user_id' in session:
+            if 'user_is_admin' in session and session['user_is_admin']:
+                return redirect(url_for("admin.home_page"))
+            else:
+                return redirect(url_for("doctor.home_page"))
+        return view(**kwargs)
+    return wrapped_view
+
+
+@logout_required
 def login_page():
     return render_template("login.html")
