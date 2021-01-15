7cda16a7066df01e3728c575f6f383a4cccd30cd
Emek Gözlüklü <gozluklu16@itu.edu.tr>
2021-01-15 03:41:39 +0300

    fix patient login

--- smarthealth_web/patient.py
+++ smarthealth_web/patient.py
@@ -1,5 +1,5 @@
 from flask import session, request, redirect, url_for, render_template, Blueprint, g
-from smarthealth_web.dboperations import query, query_where
+from smarthealth_web.dboperations import query, query_where, get_patient_by_nid
 from smarthealth_web.forms import PatientLoginForm
 from decouple import config
 import psycopg2 as dpapi2
@@ -22,19 +22,19 @@ def login():
     if form.validate_on_submit():
         # Add user validation code here
         p_national_id = form.national_id.data
-        p = query_where(table_name="patient", condition=f"national_id='{p_national_id}'")
+        p = get_patient_by_nid(p_national_id)
         if p:
             try:
                 lc = int(form.logcode.data)
             except ValueError:
                 lc = None
-            if lc == p[0][6]:
-                session["patient"] = p[0]
+            if lc == p[5]:
+                session["patient"] = p
                 return redirect(url_for("patient.home_page"))  # if successful
 
         return render_template(
             'patient/patient_login.html',
             form=form,
-            errors="Invalid credentials.",
+            errors=f"Invalid credentials.",
         )
     return render_template('patient/patient_login.html', form=form)
